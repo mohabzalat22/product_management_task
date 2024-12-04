87eaed80cec99feedf130ea8f597668e70c44709
mohabzalat <mohabzalat20@gmail.com>
2024-12-04 02:53:21 +0200

    making register controller.

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
+class RegisterController extends Controller
+{
+    public function store(RegisterRequest $request){
+        $validated = $request->validated();
+
+        // register logic
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+
+        ]);
+        
+        //login logic
+        Auth::login($user);
+
+        // redirect to dashboard
+
+        return redirect()->route('dasboard')->with('success','User Created Successfully');
+    }
+}
