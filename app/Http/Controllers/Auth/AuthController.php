b4d42bcb38c8bdb1a7e05c56324ac6116c78d0b0
mohabzalat <mohabzalat20@gmail.com>
2024-12-04 14:00:48 +0200

    adding login and logout logic.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,10 +3,34 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request){
+
+        $validated = $request->validated();
+        // login logic
+
+        if(Auth::attempt($validated)){
+            return redirect()->route('dashboard')->with('success', 'Logged In Successfully!');
+        }
+        return redirect()->route('dashboard')->with('error', 'Loged In Failed!');
+    }
+
+    public function logout(){
+        
+        Auth::logout();
+
+        // invalidate token
+        request()->session()->invalidate();
+        
+        // regenerate token
+        request()->session()->regenerateToken();
+
+        //redirect to home page
+        return redirect()->route('home');
+    }
 }
