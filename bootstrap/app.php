38446e665df181abe5716716acb1567ca9771229
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 13:02:28 +0200

    new configuration for multi purpose apps

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->statefulApi(); // important
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
