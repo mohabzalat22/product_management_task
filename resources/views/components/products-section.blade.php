a91c8402df73d9dab9240a36bd1109a6ebfe55db
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:57:33 +0200

    making products section

---
+++ resources/views/components/products-section.blade.php
@@ -0,0 +1,5 @@
+<div class="container mx-auto">
+    <button data-modal-target="create-product" data-modal-toggle="create-product" type="button" class="text-white my-2 w-full bg-indigo-600 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium text-xl px-5 py-2.5 me-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">Create</button>
+</div>
+<div class="p-5" id="products-section">
+</div>
