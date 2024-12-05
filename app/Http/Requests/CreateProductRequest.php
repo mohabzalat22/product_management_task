1f8815b77120c9307f71efa16a358dacfdfb11e2
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 13:00:03 +0200

    making create request

---
+++ app/Http/Requests/CreateProductRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Traits\ApiResponseTrait;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateProductRequest extends FormRequest
+{
+    use ApiResponseTrait;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'price' => ['required', 'numeric'],
+            'quantity' => ['required', 'numeric'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator){
+        $errors = $validator->errors()->toArray();
+        throw new HttpResponseException(
+            response()->json($this->errorResponse(message:'Error Occured During Creating New Product', data:[], status_code:422, errors:$errors), 422));
+    }
+}
