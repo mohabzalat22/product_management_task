535f45cc91a2dfeeec34320a2afd81eaeb9fef94
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:46:17 +0200

    making api routes.

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::group(['middleware'=>['auth:sanctum'], 'prefix'=>'v1'], function(){
+    Route::get('/user', function (Request $request) {return $request->user();});
+    Route::resource('products', ProductController::class)->names('products');
+});
