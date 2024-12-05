725ea3fce6053f12c85992ad3096cf105f9ff91b
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:52:49 +0200

    adding pagination buttons

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
 @section('main')
-hhh
+    @include('components.products-section')
+    <div class="container mx-auto max-w-[500px] my-2 flex">
+        <button id="prev" type="button" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Prev</button>
+        <button id="next" type="button" class="w-full py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Next</button>
+    </div>
 @endsection
+
+
