88df2c2e526fb1e9a938cd219a3f84f69fccd7ee
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:44:11 +0200

    adding product controller logic.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateProductRequest;
+use App\Http\Requests\UpdateProductRequest;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Traits\ApiResponseTrait;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
+    use ApiResponseTrait;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $products = Product::paginate(10)->withQueryString();
+        return $this->successResponse(message:'Fetched Products Successfully.', data:$products, status_code:200);
     }
 
     /**
@@ -25,9 +33,20 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateProductRequest $request)
     {
-        //
+        $validated = $request->validated();
+        
+        // create new product
+        $createdProduct = Product::create([
+            'user_id' => Auth::user()->id,
+            'name' => $validated['name'],
+            'description' =>$validated['description'],
+            'price' => $validated['price'],
+            'quantity' => $validated['quantity'],
+        ]);
+
+        return $this->successResponse(message:'Product Created Successfully.', data:$createdProduct, status_code:201);
     }
 
     /**
@@ -35,7 +54,8 @@ class ProductController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $productById = Product::find($id);
+        return $this->successResponse(message:'Fetched Specific Product Successfully.', data:$productById, status_code:200);
     }
 
     /**
@@ -49,9 +69,23 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProductRequest $request, string $id)
     {
-        //
+        $validated = $request->validated();
+        $product = Product::find($id);
+
+        if($product){
+            $updatedProduct = $product->update([
+                'name' => $validated['name'],
+                'description' =>$validated['description'],
+                'price' => $validated['price'],
+                'quantity' => $validated['quantity'],
+            ]);
+
+            if($updatedProduct){
+                return $this->successResponse(message:'Product Updated Successfully.', data:$product, status_code:200);
+            }
+        }
     }
 
     /**
@@ -59,6 +93,13 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $deletedProduct = Product::find($id);
+
+        if($deletedProduct){
+            $deletedProduct->delete();
+            return $this->successResponse(message:'Product Deleted Successfully.', data:$deletedProduct, status_code:200);
+        }
+
+        return $this->errorResponse(message:'Unable To Delete Product', data:[], status_code:422, errors:[]);
     }
 }
