2c1cf20923959e58047816cd9c9ec0c0c3e74b2f
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:46:39 +0200

    making web routes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ModifiedAuth;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return view('welcome');})->name('home');
+
+Route::group(['middleware'=> ['web', ModifiedAuth::class]], function(){
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
 });
+
+Route::group(['middleware'=> ['web', 'guest']], function(){
+    Route::controller(AuthController::class)->group(function(){
+        Route::post('login', 'login')->name('login');
+        Route::post('logout', 'logout')->name('logout');
+    });
+    Route::post('register', [RegisterController::class, 'store'])->name('register.store');
+});    
