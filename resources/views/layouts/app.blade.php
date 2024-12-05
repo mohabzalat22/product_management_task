4a423e94d4f5cf9535a666c3a1cfec81bb03318d
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:53:36 +0200

    adding csrf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>product management</title>
         @vite(['resources/css/app.css','resources/js/app.js'])
 
     </head>
     <body class="font-sans antialiased dark:bg-black dark:text-white/50">
         @include('components.navbar')
-        <div class="container mx-auto">
+        <div class="container-lg mx-auto">
             @yield('main')
         </div>
     </body>
