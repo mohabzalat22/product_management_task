ec1938a199c9f0abe8d033ed6f8d4e81d65a85e4
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:55:42 +0200

    adding welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts.app')
 @section('main')
-main
+
+<div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <h1 class="text-6xl font-extrabold text-indigo-600">
+      Welcome to Our Website!
+    </h1>
+  </div>
+
+
 @endsection
