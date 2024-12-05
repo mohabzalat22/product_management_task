477557de86f0e48692f40f556f66a910c6920734
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:57:17 +0200

    making edit  product page

---
+++ resources/views/components/product-edit-modal.php
@@ -0,0 +1,31 @@
+<div id="product" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal body -->
+            <div class="p-4 md:p-5 space-y-4">
+                <form action="" id = "updateForm">
+                    <div class="my-1">
+                        <label for="name" class="block text-xl mb-2  font-medium text-gray-900 dark:text-white">Name</label>
+                        <input name = "name" type="text" id="product-name" class="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                    </div>
+                    <div class="my-1">
+                        <label for="price" class="block text-xl mb-2  font-medium text-gray-900 dark:text-white">Price</label>
+                        <input  name = "price" type="text" id="product-price" class="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                    </div>
+                    <div class="my-1">
+                        <label for="quantitiy" class="block text-xl mb-2  font-medium text-gray-900 dark:text-white">Quantitiy</label>
+                        <input name="quantity" type="text" id="product-quantity" class="bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                    </div>
+
+                    <div class="my-2">
+                        <label for="message" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Description</label>
+                        <textarea name="description" id="product-description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write here"></textarea>
+                    </div>
+                    <button type="submit" class="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
+                </form>
+         
+            </div>
+        </div>
+    </div>
+</div>
