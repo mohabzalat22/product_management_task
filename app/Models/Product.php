919c6ffe629788687fb9f31c617c875c54b3ae21
mohabzalat <mohabzalat20@gmail.com>
2024-12-05 12:44:40 +0200

    making product model fillable.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'user_id',
+        'price',
+        'quantity',
+        'description'
+    ]; 
 }
